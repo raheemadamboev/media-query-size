4ca11fa28c6abbdd872f5085b478898b506ed345
Raheem <raheemideadev@gmail.com>
2021-09-28 00:18:52 +0500

    spacer added

--- app/src/main/java/xyz/teamgravity/mediaquerysize/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/mediaquerysize/MainActivity.kt
@@ -4,8 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,7 +14,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
                 Text(
                     text = "Raheem",
                     modifier = Modifier
@@ -27,6 +30,7 @@ class MainActivity : ComponentActivity() {
                 )
 
                 MediaQuery(comparator = Dimensions.Height lessThan 400.dp) {
+                    Spacer(modifier = Modifier.height(16.dp))
                     Text(text = "I'm only shown below a height of 400 dp")
                 }
             }
